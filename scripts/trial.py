51d6e63cbb03c1affb611190d38b332ec2d9fc32
kaku <chokurei>
2017-11-14 14:58:41 +0900

    new version

--- scripts/trial.py
+++ scripts/trial.py
@@ -20,28 +20,35 @@ def tokenize(text):
     
     return wakachi.parse(text)
 
+
+def wage_without_month(wage):
+    
+    wage_split = []
+    for idx in range(len(wage)):
+        wage_split_one = tokenize(wage[idx]).split(' ')
+        wage_split.append(wage_split_one)
+    
+    del_idx = []
+    wage_no_month = []
+    for idx in range(len(wage_split)):
+        wage_split_one = wage_split[idx] 
+        if ('月' or '月給' in wage_split_one) and ('時給' not in wage_split_one):
+            del_idx.append(idx)
+        else:
+            wage_no_month.append(wage_split_one)
+    return wage_no_month, del_idx
+
+
 data_path = '../data/wage_analysis_ver02.csv'
-
 data = pd.read_csv(data_path, encoding = 'shift_jis')
-
 data_todo = data[['Unnamed: 0','rqmt_cmpny_nm_txt', 'sal_txt']]
 data_todo.columns = [['ID','name', 'sal']]
 
 wage = list(data_todo.values[:,-1])
 
-wage_split = []
-for idx in range(len(wage)):
-    wage_split_one = tokenize(wage[idx]).split(' ')
-    wage_split.append(wage_split_one)
+wage_hourly, del_idx = wage_without_month(wage)
 
-del_idx = []
-wage_without_month = []
-for idx in range(len(wage_split)):
-    wage_split_one = wage_split[idx] 
-    if ('月' or '月給' in wage_split_one) and ('時給' not in wage_split_one):
-        del_idx.append(idx)
-    else:
-        wage_without_month.append(wage_split_one)
+
         
     
         
