ee3fa9cb89e4dc86d946c7babf4495b291015312
Chokurei <guozhilingty@gmail.com>
2017-11-14 13:40:29 +0900

    trial

---
+++ scripts/trial.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 14 10:36:17 2017
+
+@author: kaku
+"""
+
+import pandas as pd
+import MeCab
+
+wakachi = MeCab.Tagger("-O wakati")
+def tokenize(text):
+    """Retrun tokenized (Japanese Wakachi-Gaki) text
+
+    Parameters
+    ----------
+    text: string
+    """
+    
+    return wakachi.parse(text)
+
+data_path = '../data/wage_analysis_ver02.csv'
+
+data = pd.read_csv(data_path, encoding = 'shift_jis')
+
+data_todo = data[['Unnamed: 0','rqmt_cmpny_nm_txt', 'sal_txt']]
+data_todo.columns = [['ID','name', 'sal']]
+
+wage = list(data_todo.values[:,-1])
+
+wage_split = []
+for idx in range(len(wage)):
+    wage_split_one = tokenize(wage[idx]).split(' ')
+    wage_split.append(wage_split_one)
+
+del_idx = []
+wage_without_month = []
+for idx in range(len(wage_split)):
+    wage_split_one = wage_split[idx] 
+    if ('月' or '月給' in wage_split_one) and ('時給' not in wage_split_one):
+        del_idx.append(idx)
+    else:
+        wage_without_month.append(wage_split_one)
+        
+    
+        
+        
